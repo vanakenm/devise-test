1bbdd18d66357816c61d5c374696047671d0ff8f
vanakenm <martin.vanaken@8thcolor.com>
2014-08-29 08:41:51 +0200

    Initial test: password only

---
+++ app/controllers/secret_controller.rb
@@ -0,0 +1,6 @@
+class SecretController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviseTest::Application.config.secret_key_base = 'db67300bef34c2a5534ec6b7f065ee0a6e001a0e61f1d70d628742176481d0674cb98aa6da0b95de721b30ba6f437b6502fc301ca3f7cd4131b43a8f868eb667'
